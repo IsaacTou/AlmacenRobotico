34dc5ec987aebf10587a7a20ba08d84197730f8a
Isaac <isaacpereztouceda@gmail.com>
2026-05-24 19:34:35 -0400

    Agregando calidad al codigo producido mediante cambio de nombres de funciones y expresiones a un estandar global de ingles usando camelCase

--- Proyecto1.hs
+++ Proyecto1.hs
@@ -1,98 +1,98 @@
 type Coord = (Int, Int) -- (Fila, Columna)
 data Move = U | D | L | R deriving (Show, Eq)
-type State = (Coord, Coord, [Coord]) -- (Robot, CajaObjetivo, CajasDeBloqueo)  
+type State = (Coord, Coord, [Coord]) -- (Robot, targetBox, CajasDeBloqueo)  
 
-estaFuera :: Coord -> Bool
-estaFuera (x,y) | x < 0 || y < 0 = True
+isOut :: Coord -> Bool
+isOut (x,y) | x < 0 || y < 0 = True
                 | x > 5 || y > 5 = True
                 | otherwise = False
 
 
 initialState :: Coord -> Coord -> [Coord] -> State
-initialState robot cajaObjetivo cajasDeBloqueo
-    | robot == cajaObjetivo || estaFuera robot || estaFuera cajaObjetivo = ((-1,-1), (-1,-1), [])
-    | robot `elem` cajasDeBloqueo || cajaObjetivo `elem` cajasDeBloqueo = ((-1,-1), (-1,-1), [])
-    | invalidos cajasDeBloqueo  = ((-1,-1), (-1,-1), [])
-    | otherwise  = (robot, cajaObjetivo, cajasDeBloqueo)
+initialState robot targetBox lockBoxes
+    | robot == targetBox || isOut robot || isOut targetBox = ((-1,-1), (-1,-1), [])
+    | robot `elem` lockBoxes || targetBox `elem` lockBoxes = ((-1,-1), (-1,-1), [])
+    | invalidCoords lockBoxes  = ((-1,-1), (-1,-1), [])
+    | otherwise  = (robot, targetBox, lockBoxes)
     where 
-    invalidos :: [Coord] -> Bool
-    invalidos [] = False
-    invalidos (x:xs) 
+    invalidCoords :: [Coord] -> Bool
+    invalidCoords [] = False
+    invalidCoords (x:xs) 
         | x `elem` xs = True
-        | estaFuera x = True
-        | otherwise = invalidos xs       
+        | isOut x = True
+        | otherwise = invalidCoords xs       
 
 
-siguienteCoord :: Coord -> Move -> Coord
-siguienteCoord (x,y) U = (x-1, y) 
-siguienteCoord (x,y) D = (x+1, y) 
-siguienteCoord (x,y) L = (x, y-1) 
-siguienteCoord (x,y) R = (x, y+1)
+nextCoord :: Coord -> Move -> Coord
+nextCoord (x,y) U = (x-1, y) 
+nextCoord (x,y) D = (x+1, y) 
+nextCoord (x,y) L = (x, y-1) 
+nextCoord (x,y) R = (x, y+1)
 
 
 isValidMove :: State -> Move -> Bool
-isValidMove ((x,y),cajaObjetivo,cajasDeBloqueo) movimiento =
-    esValido (siguienteCoord (x,y) movimiento) cajaObjetivo cajasDeBloqueo movimiento
+isValidMove ((x,y),targetBox,lockBoxes) movement =
+    isValid (nextCoord (x,y) movement) targetBox lockBoxes movement
     where
-        esValido :: Coord -> Coord -> [Coord] -> Move -> Bool
-        esValido (nx,ny) obj bloq mov  
+        isValid :: Coord -> Coord -> [Coord] -> Move -> Bool
+        isValid (nx,ny) obj bloq mov  
 
-            | estaFuera (nx,ny) = False
-            | (nx,ny) == obj = empujeValido (siguienteCoord (nx,ny) mov) obj bloq 
-            | (nx,ny) `elem` bloq = empujeValido (siguienteCoord (nx,ny) mov) obj bloq
+            | isOut (nx,ny) = False
+            | (nx,ny) == obj = validPush (nextCoord (nx,ny) mov) obj bloq 
+            | (nx,ny) `elem` bloq = validPush (nextCoord (nx,ny) mov) obj bloq
             | otherwise = True
 
-        empujeValido :: Coord -> Coord -> [Coord] -> Bool 
-        empujeValido (cx,cy) obj bloq
-            | estaFuera (cx,cy) = False
+        validPush :: Coord -> Coord -> [Coord] -> Bool 
+        validPush (cx,cy) obj bloq
+            | isOut (cx,cy) = False
             | (cx,cy) `elem` bloq = False
             | (cx,cy) == obj = False
             | otherwise = True
 
 
 applyMove :: State -> Move -> State
-applyMove ((x,y),cajaObjetivo,cajasDeBloqueo) movimiento
-    | nuevaPos `elem` cajasDeBloqueo = modificarPosicion nuevaPos cajaObjetivo cajasDeBloqueo movimiento
-    | nuevaPos == cajaObjetivo = (nuevaPos, siguienteCoord nuevaPos movimiento, cajasDeBloqueo)
-    | otherwise = (nuevaPos, cajaObjetivo, cajasDeBloqueo)
+applyMove ((x,y),targetBox,lockBoxes) movement
+    | newPosition `elem` lockBoxes = changePosition newPosition targetBox lockBoxes movement
+    | newPosition == targetBox = (newPosition, nextCoord newPosition movement, lockBoxes)
+    | otherwise = (newPosition, targetBox, lockBoxes)
     where 
-        nuevaPos = siguienteCoord(x,y) movimiento
+        newPosition = nextCoord(x,y) movement
 
-        modificarPosicion :: Coord -> Coord -> [Coord] -> Move -> State
-        modificarPosicion (rx,ry) cajaobj cajasbloq mov = ((rx,ry),cajaobj,nuevaCajasBloq)
+        changePosition :: Coord -> Coord -> [Coord] -> Move -> State
+        changePosition (rx,ry) objBox lockBoxes mov = ((rx,ry),objBox,newLockBoxes)
             where
-                cajasBloqModificado = filter(/= (rx,ry)) cajasbloq
-                nuevaCajasBloq = siguienteCoord (rx,ry) mov:cajasBloqModificado
+                changeLockBoxes = filter(/= (rx,ry)) lockBoxes
+                newLockBoxes = nextCoord (rx,ry) mov:changeLockBoxes
 
 
-agregarNodos :: (State, Int, [State]) -> [(State, Int, [State])]
-agregarNodos (st, nivel, camino) = 
-    intentar U ++ intentar D ++ intentar L ++ intentar R
+addNodes :: (State, Int, [State]) -> [(State, Int, [State])]
+addNodes (st, level, road) = 
+    tryMov U ++ tryMov D ++ tryMov L ++ tryMov R
     where
-    intentar :: Move -> [(State, Int, [State])]
-    intentar mov
+    tryMov :: Move -> [(State, Int, [State])]
+    tryMov mov
         | isValidMove st mov = 
             let nuevoSt = applyMove st mov
-            in [(nuevoSt, nivel + 1, nuevoSt : camino)] 
+            in [(nuevoSt, level + 1, nuevoSt : road)] 
         | otherwise = []    
 
 
-esEstadoFinal :: State -> Bool
-esEstadoFinal (robotCoord, (cx,cy), cajasbloq)
+isFinalState :: State -> Bool
+isFinalState (robotCoord, (cx,cy), cajasbloq)
     | cx == 5 && cy == 5 = True 
     | otherwise = False
 
 
 solveWarehouse :: State -> (Int, [State])
-solveWarehouse estadoInicial = bfs [] [(estadoInicial, 0, [estadoInicial])]
+solveWarehouse initialState = bfs [] [(initialState, 0, [initialState])]
     where
         bfs :: [State] -> [(State, Int, [State])] -> (Int, [State])
         bfs _ [] = (0, []) 
-        bfs visitados ((st, nivel, camino) : colaRestante)
-            | esEstadoFinal st = (nivel, reverse camino) 
-            | st `elem` visitados = bfs visitados colaRestante
+        bfs visited ((st, level, road) : residuaryTail)
+            | isFinalState st = (level, reverse road) 
+            | st `elem` visited = bfs visited residuaryTail
             | otherwise = 
-            let nuevosHijos = agregarNodos (st, nivel, camino)
-                nuevaCola = colaRestante ++ nuevosHijos 
-                nuevosVisitados = st : visitados        
-            in bfs nuevosVisitados nuevaCola
+            let newChildrens = addNodes (st, level, road)
+                newTail = residuaryTail ++ newChildrens 
+                newVisited = st : visited        
+            in bfs newVisited newTail
