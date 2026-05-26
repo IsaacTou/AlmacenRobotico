15f415ecd54d658dfbf096afce6ada978347a3d3
Maryelen13 <memducv@gmail.com>
2026-05-25 23:13:27 -0400

    comentarios y readme

--- Proyecto1.hs
+++ Proyecto1.hs
@@ -1,8 +1,8 @@
 type Coord = (Int, Int) -- (Fila, Columna)
 data Move = U | D | L | R deriving (Show, Eq)
 type State = (Coord, Coord, [Coord]) -- (Robot, CajaObjetivo, CajasDeBloqueo)  
-
-estaFuera :: Coord -> Bool
+--Parte 1
+estaFuera :: Coord -> Bool-- funcion para saber si una coordenada se salio del tablero
 estaFuera (x,y) | x < 0 || y < 0 = True
                 | x > 5 || y > 5 = True
                 | otherwise = False
@@ -23,18 +23,18 @@ initialState robot cajaObjetivo cajasDeBloqueo
         | otherwise = invalidos xs       
 
 
-siguienteCoord :: Coord -> Move -> Coord
+siguienteCoord :: Coord -> Move -> Coord--funcion para obtener la siguiente coordenada dependiendodel movimiento que se quiere realizar
 siguienteCoord (x,y) U = (x-1, y) 
 siguienteCoord (x,y) D = (x+1, y) 
 siguienteCoord (x,y) L = (x, y-1) 
 siguienteCoord (x,y) R = (x, y+1)
-
+--Parte 2
 
 isValidMove :: State -> Move -> Bool
 isValidMove ((x,y),cajaObjetivo,cajasDeBloqueo) movimiento =
     esValido (siguienteCoord (x,y) movimiento) cajaObjetivo cajasDeBloqueo movimiento
     where
-        esValido :: Coord -> Coord -> [Coord] -> Move -> Bool
+        esValido :: Coord -> Coord -> [Coord] -> Move -> Bool-- funcion donde validamos si el movimiento es valido o no
         esValido (nx,ny) obj bloq mov  
 
             | estaFuera (nx,ny) = False
@@ -42,14 +42,14 @@ isValidMove ((x,y),cajaObjetivo,cajasDeBloqueo) movimiento =
             | (nx,ny) `elem` bloq = empujeValido (siguienteCoord (nx,ny) mov) obj bloq
             | otherwise = True
 
-        empujeValido :: Coord -> Coord -> [Coord] -> Bool 
+        empujeValido :: Coord -> Coord -> [Coord] -> Bool --funcion donde evaluamos si el empuje se puede realizar o no 
         empujeValido (cx,cy) obj bloq
             | estaFuera (cx,cy) = False
             | (cx,cy) `elem` bloq = False
             | (cx,cy) == obj = False
             | otherwise = True
 
-
+--Parte 3
 applyMove :: State -> Move -> State
 applyMove ((x,y),cajaObjetivo,cajasDeBloqueo) movimiento
     | nuevaPos `elem` cajasDeBloqueo = modificarPosicion nuevaPos cajaObjetivo cajasDeBloqueo movimiento
@@ -61,10 +61,10 @@ applyMove ((x,y),cajaObjetivo,cajasDeBloqueo) movimiento
         modificarPosicion :: Coord -> Coord -> [Coord] -> Move -> State
         modificarPosicion (rx,ry) cajaobj cajasbloq mov = ((rx,ry),cajaobj,nuevaCajasBloq)
             where
-                cajasBloqModificado = filter(/= (rx,ry)) cajasbloq
+                cajasBloqModificado = filter(/= (rx,ry)) cajasbloq --utlilizamos filter para eliminar la caja de bloqueo que se va a mover y luego agregamos la nueva posiscion de dicha caja ya movida
                 nuevaCajasBloq = siguienteCoord (rx,ry) mov:cajasBloqModificado
 
-
+--Parte 4
 agregarNodos :: (State, Int, [State]) -> [(State, Int, [State])]
 agregarNodos (st, nivel, camino) = 
     intentar U ++ intentar D ++ intentar L ++ intentar R
@@ -72,12 +72,12 @@ agregarNodos (st, nivel, camino) =
     intentar :: Move -> [(State, Int, [State])]
     intentar mov
         | isValidMove st mov = 
-            let nuevoSt = applyMove st mov
+            let nuevoSt = applyMove st mov--hacemos uso del let e in para hacer uso de variable temporal que nos permite guardar el estado nuevo 
             in [(nuevoSt, nivel + 1, nuevoSt : camino)] 
         | otherwise = []    
 
 
-esEstadoFinal :: State -> Bool
+esEstadoFinal :: State -> Bool--vemos si llego a la meta o no
 esEstadoFinal (robotCoord, (cx,cy), cajasbloq)
     | cx == 5 && cy == 5 = True 
     | otherwise = False
@@ -92,7 +92,7 @@ solveWarehouse estadoInicial = bfs [] [(estadoInicial, 0, [estadoInicial])]
             | esEstadoFinal st = (nivel, reverse camino) 
             | st `elem` visitados = bfs visitados colaRestante
             | otherwise = 
-            let nuevosHijos = agregarNodos (st, nivel, camino)
+            let nuevosHijos = agregarNodos (st, nivel, camino)--usamos let e in de neuvo para agg los hijos a la cola
                 nuevaCola = colaRestante ++ nuevosHijos 
                 nuevosVisitados = st : visitados        
             in bfs nuevosVisitados nuevaCola
---
+++ README.md
@@ -0,0 +1,25 @@
+# Proyecto #1: Almacén Robótico
+
+### Datos de los Integrantes
+* **Nombres:** Maria Elena Medina Diaz, Isaac Perez Touceda
+* **Cédulas de Identidad:** 32.061.879, 
+* **Asignatura:** Lenguajes de Programación 
+* **Docente:** Eugenio Scalise 
+
+
+## Explicación de Funciones y Herramientas utilizadas
+
+para la resolucion de el proyecto solo utilizamos operaciones del Prelude de Haskell, por lo tanto a continuacion detallare algunas de las funciones mas destacadas que utilizamos debido a que nos eran de  gran utilidad:
+
+### `elem`
+Esta función toma un objeto y una lista de objetos, y nos indica si ese objeto pertenece o no a la lista. Generalmente se utiliza como una función infija (colocándola en medio de los argumentos) porque así es mucho más fácil de leer en el código.  La aplicamos en casi todo el proyecto: en `initialState` e `invalidos` para revisar que los obstáculos no se solapen; en `esValido` y `empujeValido` para chequear si el robot chocó con alguna caja; en `applyMove` para saber si la casilla destino tiene un obstáculo; y finalmente en `solveWarehouse` dentro del BFS para verificar si el estado actual ya pertenece a la lista de `visitados` y evitar ciclos infinitos.
+
+### `let, in`
+Esta herramienta nos permite declarar variables temporales que solo existen dentro de una función específica. Con el `let` guardamos los datos que necesitemos y luego, por medio del `in`, podemos utilizar lo que se guardó en esas variables para procesar el resultado.La implementamos en la función `agregarNodos`. Nos sirvió para crear la variable local `nuevoSt` que almacena el resultado de `applyMove st mov`. Luego, en el bloque `in`, usamos esa variable para armar y devolver la tupla correspondiente con el nivel incrementado y el camino actualizado. Ademas en la función `solveWarehouse`utilizamos el let para calcular los nuevosHijos, la actualización de la cola (nuevaCola) y el registro del estado actual en la lista de visitados (nuevosVisitados). Luego, usando el in, le pasamos esos datos temporales a la siguiente llamada recursiva del bfs.
+
+
+## Expresión `deriving (Show, Eq)`
+ la cláusula `deriving` le permite a Haskell generar de forma automática el comportamiento de ciertas clases de tipos básicas sin que nosotros tengamos que programar las funciones desde cero.
+
+* ** `Eq` (Equality):** Se utiliza para los tipos de datos cuyos valores pueden ser comparados por igualdad. Al colocarla, el compilador nos proporciona los operadores `(==)` y `(/=)`. En nuestro proyecto es vital, ya que al aplicarla en nuestro tipo `Move`, permite que funciones como `elem` puedan comparar los estados del robot y sus movimientos de forma directa.
+* ** `Show`:** Se utiliza para los tipos de datos cuyos valores necesitamos convertir en una cadena de caracteres (`String`). En nuestro código la añadimos al tipo `Move` para poder transformar los constructores `U`, `D`, `L`, `R` en texto real y así mostrar el camino de la solución en pantalla.
